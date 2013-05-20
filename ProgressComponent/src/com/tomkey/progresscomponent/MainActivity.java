d824456c6918c694cf9fb495ada53a7f16b35166
tomkeyzhang <tomkeyzhang@gmail.com>
2013-05-20 13:30:01 +0800

    添加ProgressActivity和ProgressFragment

--- ProgressComponent/src/com/tomkey/progresscomponent/MainActivity.java
+++ ProgressComponent/src/com/tomkey/progresscomponent/MainActivity.java
@@ -1,20 +1,29 @@
 package com.tomkey.progresscomponent;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends ProgressActivity {
+    Handler handler=new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handler.postDelayed(new Runnable() {
+            
+            @Override
+            public void run() {
+                setContentEmpty(true);
+                setEmptyText("空");
+                setContentShown(true);
+            }
+        }, 3000);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
